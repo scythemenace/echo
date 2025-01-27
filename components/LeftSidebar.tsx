a52940b229ed2f6c30b0e4f5dfa9b410c12ce2be
Ankur Pandey <pankur216@gmail.com>
2025-01-26 20:59:04 -0500

    Prelim code for left sidebar and separated mapping data in the constants folder

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,10 +1,34 @@
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 /*We'll use this section as a navigation component*/
 const LeftSidebar = () => {
   return (
-    <section>
-      <nav></nav>
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        {/*Link to homepage*/}
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
+          <Image
+            src="/icons/logo.png"
+            alt="logo"
+            width={30}
+            height={30}
+          ></Image>
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            Echo
+          </h1>
+        </Link>
+
+        {sidebarLinks.map(({ route, label }) => {
+          // eslint-disable-next-line react/jsx-key
+          return <Link href={route}>{label}</Link>;
+        })}
+      </nav>
     </section>
   );
 };
