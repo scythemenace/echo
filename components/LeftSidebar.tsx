77031e791c0394e4fb7e2b02869958ec9ed7f43e
Ankur Pandey <pankur216@gmail.com>
2025-01-26 19:27:48 -0500

    Init LeftSidebar

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/*We'll use this section as a navigation component*/
+const LeftSidebar = () => {
+  return (
+    <section>
+      <nav></nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
