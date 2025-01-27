8c7ffd96c5a047a94e614fbf15f9ceee36065800
Ankur Pandey <pankur216@gmail.com>
2025-01-26 21:19:55 -0500

    LeftSidebar - proper placement

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,10 +7,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
         <LeftSidebar />
-        {children}
+
+        <section>
+          <div>
+            <div>
+              <Image />
+              MobileNav
+            </div>
+            Toaster (notification popups)
+            {children}
+          </div>
+        </section>
         <p className="text-white-1">RIGHT SIDEBAR</p>
       </main>
     </div>
--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,10 +1,17 @@
+"use client"; /*Without this it'd give an error since it's trying to render it on the server side*/
+
 import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 /*We'll use this section as a navigation component*/
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
@@ -13,20 +20,31 @@ const LeftSidebar = () => {
           href="/"
           className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
         >
-          <Image
-            src="/icons/logo.png"
-            alt="logo"
-            width={30}
-            height={30}
-          ></Image>
+          <Image src="/icons/logo.png" alt="logo" width={30} height={30} />
           <h1 className="text-24 font-extrabold text-white max-lg:hidden">
             Echo
           </h1>
         </Link>
 
-        {sidebarLinks.map(({ route, label }) => {
-          // eslint-disable-next-line react/jsx-key
-          return <Link href={route}>{label}</Link>;
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                {
+                  "bg-nav-focus border-r-4 border-orange-1": isActive,
+                },
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
         })}
       </nav>
     </section>
