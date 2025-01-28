d7985c6f0e34d3f566f4f230203f4e6eafe299aa
Ankur Pandey <pankur216@gmail.com>
2025-01-27 19:02:39 -0500

    organized images in grid, added some css to images using global.css class

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,15 +7,17 @@ const Home = () => {
     <div className="mt-9 flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Audiobooks</h1>
-        {audiobookData.map(({ id, title, description, imgURL }) => (
-          <AudiobookCard
-            key={id}
-            title={title}
-            description={description}
-            imgUrl={imgURL}
-            audiobookId={id}
-          />
-        ))}
+        <div className="audiobook_grid">
+          {audiobookData.map(({ id, title, description, imgURL }) => (
+            <AudiobookCard
+              key={id}
+              title={title}
+              description={description}
+              imgUrl={imgURL}
+              audiobookId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
